bbff3bd054dfe5e4c96385e6f104b0a3cfaff968
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-20 10:16:49 +0700

    Commit code

---
+++ PRN_SE1621_PRODUCT_MANAGEMENT/Manager/ProductManagement.cs
@@ -0,0 +1,33 @@
+﻿namespace Prn.Se1621;
+public class ProductManagement : AbsProduct, IProduct
+{
+    public void Add(Product p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(Product p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product Get(Product p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product[] GetAllProductByUnitPrice(double price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Product p)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PRN_SE1621_PRODUCT_MANAGEMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Prn.Se1621;
+using static System.Console;
+
+public class Program
+{
+    public static void Main()
+    {
+        Product p = new Product();
+        p.Id = 1;
+        p.ProductName = "Laptop Dell";
+        p.Desc = "San pham moi nhat 2022";
+        p.UnitPrice = 1500;
+        p.StartDate = DateTime.Now;//current date
+
+
+
+        WriteLine(p);
+        ReadLine(); 
+    }
+}
+
+
