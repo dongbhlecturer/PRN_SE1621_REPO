3df7088c5c55176099643b04b4d1bda67109399c
DONGBHPC\Dong Bui Huu <buihuudong19@gmail.com>
2022-05-20 09:08:14 +0700

    Add project files.

---
+++ PRN_SE1621_DEFAULT_INTERFACE/Program.cs
@@ -0,0 +1,88 @@
+﻿
+namespace PRN.SE1621.DEFAULT_INTERFACE;
+using System.Collections;
+using static System.Console;
+/*define interface*/
+
+public interface ISample
+{
+   static void Print()
+   {
+        WriteLine($"Hello .NET C# 10");
+   }
+    string GetString(string s)
+    {
+        return "Welcome " + s;
+    }
+    void Display();
+}
+
+public interface ISecond
+{
+    string GetString(string s)
+    {
+        return "Goodmorning " + s;
+    }
+}
+
+public class MySecond : ISecond
+{
+
+}
+public class MySample : ISample
+{
+
+    public void Display()
+    {
+        WriteLine($" Hi !");
+    }
+}
+
+
+public class MyClass
+{
+    public static int x = 1;
+    static MyClass()
+    {
+        x = 2;
+        WriteLine($"Static constructor {x}");
+    }
+
+    public MyClass()
+    {
+        x++; //x=x+1;
+        WriteLine($"public constructor {x}");
+    }
+
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        /*
+        MySample obj = new MySample(); //instance (hien thuc cua class MySample
+        obj.Display();
+        
+
+        ISample.Print();
+      
+        ISample s = obj;
+        string str = s.GetString("Dong");
+        WriteLine(str);
+
+        ISecond a = new MySecond();
+        str = a.GetString("abc");
+        WriteLine(str);
+
+        */
+        MyClass m1 = new MyClass();
+        MyClass.x = 4;
+        MyClass m2 = new MyClass();
+        ReadLine();
+        
+
+        
+    }
+}
---
+++ PRN_SE1621_PRO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Fptedu.Prn1621;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"==================================");
+        int a = 10, b = 20; //argruments
+        //Console.WriteLine("Total of a = {0} and b= {1} is {2}",a,b, Sum(a,b));
+        Console.WriteLine($"Total of a = {a} and b = {b} is: {Sum(a,b)}"); //String Interpolation
+        //Console.WriteLine(args[0]);
+
+        Console.ReadLine();
+    }
+    //manual methods: parameters
+    /*
+        multi-lines
+     */
+    public static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+}
